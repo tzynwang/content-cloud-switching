de6ac4e1be987cc42fc1ed93f3ca0c46a5cff62d
Charlie <charlie@lawsnote.com>
2022-06-09 15:34:21 +0800

    [feat] Implement switch feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { memo, useState, useEffect } from 'react';
+import cn from 'classnames';
 import './App.css';
 
+enum STEPS {
+  ONE = 'ONE',
+  TWO = 'TWO',
+  THREE = 'THREE',
+}
+
 function App() {
+  /* States */
+  const [step, setStep] = useState<STEPS>(STEPS.ONE);
+
+  /* Functions */
+  const handleStepChange = (currentStep: STEPS) => () => {
+    setStep(currentStep);
+  };
+
+  /* Hooks */
+  useEffect(() => {}, [step]);
+
+  /* Main */
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Wrapper">
+        <div className={cn('container', `container${step}`)}>
+          <div className="item">
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+          </div>
+          <div className="item">
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+          </div>
+          <div className="item">
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+            <span className="tag">標籤</span>
+          </div>
+        </div>
+      </div>
+      <div className="ButtonWrapper">
+        <button
+          className={cn('stepButton', step === STEPS.ONE && 'currentButton')}
+          onClick={handleStepChange(STEPS.ONE)}
+        ></button>
+        <button
+          className={cn('stepButton', step === STEPS.TWO && 'currentButton')}
+          onClick={handleStepChange(STEPS.TWO)}
+        ></button>
+        <button
+          className={cn('stepButton', step === STEPS.THREE && 'currentButton')}
+          onClick={handleStepChange(STEPS.THREE)}
+        ></button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default memo(App);
